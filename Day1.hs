1aa53b56fd9a25b576f05cfdade48cadbe23ec32
Kudostoy0u <kudosbeluga@gmail.com>
2021-12-05 18:39:31 +0000

    part 1 and part 2 solved

--- Day1.hs
+++ Day1.hs
@@ -1,4 +1,4 @@
 module Main where
-
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+p2 = True -- Toggle whether you're trying to solve part 1 or 2
+day1 x y z = if x < length z then day1 (x+1) (y ++ [(if p2 then z!!x + z!!(x-1) + z!!(x-2) else z!!x) - (if p2 then z!!(x-1) + z!!(x-2) + z!!(x-3) else z !! (x-1))]) z else length $ filter (> 0) y
+main = readFile "input/Day1.txt" >>= print . day1 (if p2 then 4 else 1) [] . map (\x-> read x :: Int) . lines
---
+++ Day2.hs
@@ -0,0 +1,10 @@
+module Main where
+p2 = True -- toggle whether part 1 or part 2
+day2 idx x y aim list
+    | cmd == "forward" = day2 (idx+1) (x+cord) (if p2 then (y + aim*cord) else y) aim list
+    | cmd == "down" = day2 (idx+1) x (if p2 then y else (y+cord)) (if p2 then (aim+cord) else aim) list
+    | cmd == "up" = day2 (idx+1) x (if p2 then y else (y-cord)) (if p2 then (aim-cord) else aim) list
+    | otherwise = x*y
+    where cmd = if (idx < length list) then list !! idx !! 0 else "0"
+          cord = if (idx < length list) then read (list !! idx !! 1) :: Int else 0
+main = readFile "input/Day2.txt" >>= print . day2 0 0 0 0 . map words . lines
