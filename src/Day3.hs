fd3730902483dfab2a3ce12db3689b611417ea35
Kudos Beluga <69732000+Kudostoy0u@users.noreply.github.com>
2021-12-19 19:08:59 -0600

    Update Day3.hs

--- src/Day3.hs
+++ src/Day3.hs
@@ -11,5 +11,5 @@ convert (x : xs) = x + 2 * convert xs
 solvePart1 x = convert x * convert (map (\y -> if y == 1 then 0 else 1) x)
 filterOut arr idx isOxy = if length arr == 1 then convert $ reverse $ map digitToInt (head arr) else filterOut (filter (\x -> digitToInt (x!!idx) == (toBinary isOxy arr !! idx)) arr) (idx+1) isOxy
 solvePart2 x = let preset = filterOut x 0 in preset True * preset False
-toBinary oxy = map ( mostCommon oxy . map digitToInt) . transpose
+toBinary oxy = map (mostCommon oxy . map digitToInt) . transpose
 main = readFile "input/Day3.txt" >>= print . \x -> if part2 then solvePart2 (lines x) else solvePart1 $ reverse $ toBinary False $ lines x
