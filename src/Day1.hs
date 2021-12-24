04a48dfcf563ab077b5b2376d3cf72f737c67ab0
Kudostoy0u <kudosbeluga@gmail.com>
2021-12-24 19:29:58 +0000

    update day1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,3 +1,5 @@
+--module Main where
+-- uncomment line above if is specified to run on AOC2021.cabal
 p2 = True -- Toggle whether you're trying to solve part 1 or 2
 day1 x y z = if x < length z then day1 (x+1) (y ++ [(if p2 then z!!x + z!!(x-1) + z!!(x-2) else z!!x) - (if p2 then z!!(x-1) + z!!(x-2) + z!!(x-3) else z !! (x-1))]) z else length $ filter (> 0) y
 main = readFile "input/Day1.txt" >>= print . day1 (if p2 then 4 else 1) [] . map (\x-> read x :: Int) . lines
