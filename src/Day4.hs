30f101df2d4f7f7dfb3eb00587bd014bb7cb63c0
Kudostoy0u <kudosbeluga@gmail.com>
2021-12-24 17:51:34 +0000

    part4 working

--- src/Day4.hs
+++ src/Day4.hs
@@ -1,22 +1,17 @@
--- As problems get more complicated type annotations and syntax sugar become more essential
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Main where
---import Debug.Trace (traceShow)
-import Data.Matrix((!), fromLists, getCol, getRow, mapPos, toList, Matrix)
+import Data.Matrix((!), fromLists, getCol, getRow, mapPos, toList, Matrix,zero)
 import Data.List.Split (splitOn, chunksOf)
 import Data.List (elemIndex)
 import Helpers (strToInt)
 p2 = True -- Toggle whether part 1 or 2
-getTotal :: Matrix Int -> Int
+taker [] items = filter (/= -1) items
+taker (x:xs) items = taker xs $ take x items ++ [-1] ++ drop (x+1) items
 getTotal x = sum $ filter (>=0) $ toList x
-getIfSolved :: [Matrix Int] -> Maybe Int
-getIfSolved a = elemIndex True $ map (\z -> elem (negate 5) $ map (\y -> sum $ getRow y z) [1..5] ++ map (\y -> sum $ getCol y z) [1..5]) a
-part1 :: [Matrix Int] -> [Int] -> Int
-part1 bingoBoard (instruction:rest) = let newBoard = map (\x -> mapPos (\(row,column) a -> if x!(row,column) == instruction then -1 else a) x) bingoBoard; isSolved = getIfSolved newBoard in case isSolved of Just n -> instruction * getTotal (newBoard!!n); Nothing -> part1 newBoard rest
+getIfSolved a = map fst $ filter snd (zip [0..] (map (\z -> elem (negate 5) $ map (\y -> sum $ getRow y z) [1..5] ++ map (\y -> sum $ getCol y z) [1..5]) a) :: [(Int,Bool)])
+part1 bingoBoard (instruction:rest) = let newBoard = map (\x -> mapPos (\(row,column) a -> if x!(row,column) == instruction then -1 else a) x) bingoBoard; isSolved = getIfSolved newBoard in if not(null isSolved) then if p2&&(length bingoBoard > 1) then part1 (taker isSolved newBoard) rest else instruction * getTotal (newBoard!!head isSolved) else part1 newBoard rest
 main = do
     input <- readFile "input/Day4.txt"
     let allInput = filter (/= "") $ lines input
     let bingoInstructions = map strToInt (splitOn "," $ head allInput)
-    let bingoLines = tail allInput
-    let bingoBoards = map fromLists $ chunksOf 5 $ map (map strToInt . filter (not . null) . splitOn " ") bingoLines
+    let bingoBoards = map fromLists $ chunksOf 5 $ map (map strToInt . filter (not . null) . splitOn " ") $ tail allInput
     print $ part1 bingoBoards bingoInstructions
