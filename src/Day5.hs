299aae04ec8fcbf11693988997965ee73c3fc7a5
Kudostoy0u <kudosbeluga@gmail.com>
2021-12-25 00:37:48 +0000

    start on day 5

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,3 +1,15 @@
+{-
+NOT USING MATRIX
+Using point data create array of all points, then count the number of duplicates
+-}
 module Main where
-
-main = main
+import Data.List.Split (splitOn,chunksOf)
+import Helpers (strToInt)
+allPoints :: [[Int]] -> [Int]
+allPoints x = x!!0
+main = do
+    input <- readFile "input/Day5.txt"
+    let cords = map (allPoints . map (map strToInt . splitOn ",") . splitOn " -> ") $ lines input
+    
+    --let board = zero 1001 1001
+    print cords
