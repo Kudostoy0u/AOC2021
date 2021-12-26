0b2b964e556d808807066b7d761b62f68f0c8204
Kudostoy0u <kudosbeluga@gmail.com>
2021-12-26 18:41:25 +0000

    working on day 5

--- src/Day5.hs
+++ src/Day5.hs
@@ -4,12 +4,30 @@ Using point data create array of all points, then count the number of duplicates
 -}
 module Main where
 import Data.List.Split (splitOn,chunksOf)
-import Helpers (strToInt)
-allPoints :: [[Int]] -> [Int]
-allPoints x = x!!0
+import Data.List (group)
+import qualified Data.Map as Map
+import Helpers (strToInt,count)
+p2 = False
+allPoints :: [[Int]] -> [(Int,Int)]
+allPoints x
+  | x1 == x2 = {-vertical-} zip (repeat x1) ylist
+  | y1 == y2 = {-horizontal-} zip xlist (repeat y1)
+  | otherwise = {-diagonal-} if p2 then zip xlist ylist else [(-1,-1)]
+  where
+    first = head x
+    second = x!!1
+    x1 = head first
+    x2 = head second
+    y1 = first!!1
+    y2 = second!!1
+    xlist = if x2 > x1 then [x1..x2] else [x2..x1]
+    ylist = if y2 > y1 then [y1..y2] else [y2..y1]
+makeMap :: [(Int,Int)] -> Map.Map (Int,Int) Int -> Map.Map (Int,Int) Int
+makeMap [] theMap = theMap
+makeMap (cord:rest) theMap = makeMap rest $ Map.insertWith (+) cord 1 theMap
 main = do
     input <- readFile "input/Day5.txt"
-    let cords = map (allPoints . map (map strToInt . splitOn ",") . splitOn " -> ") $ lines input
-    
-    --let board = zero 1001 1001
-    print cords
+    let cords = filter (/=(-1,-1)) . concatMap (allPoints . map (map strToInt . splitOn ",") . splitOn " -> ") $ lines input
+    --let res = length $ filter (\x -> count x cords > 1) cords
+    --print $ res `div` 2
+    print $ makeMap cords (Map.empty)
--- src/Helpers.hs
+++ src/Helpers.hs
@@ -1,2 +1,5 @@
-module Helpers (strToInt) where
+module Helpers (strToInt,taker,count) where
 strToInt x = read x :: Int
+taker [] items = filter (/= -1) items
+taker (x:xs) items = taker xs $ take x items ++ [-1] ++ drop (x+1) items
+count x = length . filter (x==)
