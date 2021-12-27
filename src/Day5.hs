e1ed049ac389cea8ece2375350fefc8653e7286f
Kudostoy0u <kudosbeluga@gmail.com>
2021-12-27 16:59:52 +0000

    day 5 done

--- src/Day5.hs
+++ src/Day5.hs
@@ -2,7 +2,7 @@ module Main where
 import Data.List.Split (splitOn)
 import qualified Data.Map as Map
 import Helpers (strToInt)
-p2 = False
+p2 = True
 allPoints x
   | x1 == x2 = {-vertical-} zip (repeat x1) ylist
   | y1 == y2 = {-horizontal-} zip xlist (repeat y1)
@@ -16,5 +16,5 @@ allPoints x
     y2 = second!!1
     xlist = if x2>x1 then [x1..x2] else [x1,x1-1..x2]
     ylist = if y2>y1 then [y1..y2] else [y1,y1-1..y2]
-makeMap rest theMap = length $ filter (>1) $ Map.elems $ foldl (\theMap cord -> Map.insertWith (+) cord 1 theMap) theMap rest
+makeMap rest theMap = length $ filter (>1) $ Map.elems $ foldr (\cord theMap -> Map.insertWith (+) cord 1 theMap) theMap rest
 main = readFile "input/Day5.txt" >>= \x -> print $ makeMap (filter (/=(-1,-1)) . concatMap (allPoints . map (map strToInt . splitOn ",") . splitOn " -> ") $ lines x) Map.empty
