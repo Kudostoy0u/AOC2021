bb00ae18a6594012954d2ec8b6a564847f06e7af
Kudostoy0u <kudosbeluga@gmail.com>
2021-12-27 16:25:09 +0000

    got day5 working

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,14 +1,8 @@
-{-
-NOT USING MATRIX
-Using point data create array of all points, then count the number of duplicates
--}
 module Main where
-import Data.List.Split (splitOn,chunksOf)
-import Data.List (group)
+import Data.List.Split (splitOn)
 import qualified Data.Map as Map
-import Helpers (strToInt,count)
+import Helpers (strToInt)
 p2 = False
-allPoints :: [[Int]] -> [(Int,Int)]
 allPoints x
   | x1 == x2 = {-vertical-} zip (repeat x1) ylist
   | y1 == y2 = {-horizontal-} zip xlist (repeat y1)
@@ -20,14 +14,7 @@ allPoints x
     x2 = head second
     y1 = first!!1
     y2 = second!!1
-    xlist = if x2 > x1 then [x1..x2] else [x2..x1]
-    ylist = if y2 > y1 then [y1..y2] else [y2..y1]
-makeMap :: [(Int,Int)] -> Map.Map (Int,Int) Int -> Map.Map (Int,Int) Int
-makeMap [] theMap = theMap
-makeMap (cord:rest) theMap = makeMap rest $ Map.insertWith (+) cord 1 theMap
-main = do
-    input <- readFile "input/Day5.txt"
-    let cords = filter (/=(-1,-1)) . concatMap (allPoints . map (map strToInt . splitOn ",") . splitOn " -> ") $ lines input
-    --let res = length $ filter (\x -> count x cords > 1) cords
-    --print $ res `div` 2
-    print $ makeMap cords (Map.empty)
+    xlist = if x2>x1 then [x1..x2] else [x1,x1-1..x2]
+    ylist = if y2>y1 then [y1..y2] else [y1,y1-1..y2]
+makeMap rest theMap = length $ filter (>1) $ Map.elems $ foldl (\theMap cord -> Map.insertWith (+) cord 1 theMap) theMap rest
+main = readFile "input/Day5.txt" >>= \x -> print $ makeMap (filter (/=(-1,-1)) . concatMap (allPoints . map (map strToInt . splitOn ",") . splitOn " -> ") $ lines x) Map.empty
