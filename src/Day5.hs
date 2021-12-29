e337984f5b1096d61b9290834e17d84ca6de6861
Kudostoy0u <kudosbeluga@gmail.com>
2021-12-29 17:34:30 +0000

    foldl' instead of foldl

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,6 +1,6 @@
 module Day5 (day5) where
 import qualified Data.IntMap as Map
-import Helpers (strToInt,splitOn)
+import Helpers (strToInt,splitOn,foldl')
 allPoints p2 x
   | x1 == x2 = {-vertical-} zip (repeat x1) ylist
   | y1 == y2 = {-horizontal-} zip xlist (repeat y1)
@@ -14,5 +14,5 @@ allPoints p2 x
     y2 = second!!1
     xlist = if x2>x1 then [x1..x2] else [x1,x1-1..x2]
     ylist = if y2>y1 then [y1..y2] else [y1,y1-1..y2]
-solveDay5 rest theMap = length $ filter (>1) $ Map.elems $ foldl (\theMap cord -> Map.insertWith (+) cord 1 theMap) theMap rest
+solveDay5 rest theMap = length $ filter (>1) $ Map.elems $ foldl' (\theMap cord -> Map.insertWith (+) cord 1 theMap) theMap rest
 day5 p2 x = solveDay5 (map (\y->(snd y * 1000)+fst y). filter (/=(-1,-1)) . concatMap (allPoints p2 . map (map strToInt . splitOn ",") . splitOn " -> ") $ lines x) Map.empty
--- src/Helpers.hs
+++ src/Helpers.hs
@@ -1,4 +1,4 @@
-module Helpers (strToInt,taker,count,splitOn,chunksOf,group,transpose,sort,digitToInt,elemIndex) where
+module Helpers (strToInt,taker,count,splitOn,chunksOf,group,transpose,sort,digitToInt,elemIndex,foldl') where
 import Data.List.Split (splitOn,chunksOf)
 import Data.List (group,transpose,sort,elemIndex)
 import Data.Char (digitToInt)
@@ -6,3 +6,5 @@ strToInt x = read x :: Int
 taker [] items = filter (/= -1) items
 taker (x:xs) items = taker xs $ take x items ++ [-1] ++ drop (x+1) items
 count arr match = length $ filter (==match) arr
+foldl' f z [] = z
+foldl' f z (x:xs) = let z' = z `f` x in seq z' $ foldl' f z' xs
