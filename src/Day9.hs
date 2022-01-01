efe28f3ab0439618af0bfacf1abe740603e38b85
Kudostoy0u <kudosbeluga@gmail.com>
2022-01-01 22:03:39 +0000

    part 1 day 9 solved (unusually easy)

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,6 +1,9 @@
 module Day9 (day9) where
-import Helpers (digitToInt)
+import Helpers (digitToInt,fromMaybe)
 import Control.Arrow as Bi
 import qualified Data.Map as Map
-day9 p2 input = Map.lookup (1,1) $ Map.fromList $ concatMap ((\line -> map (\x->((fst x,fst line),digitToInt $ snd x)) $ snd line) . Bi.second (zip [0..])) $ zip [0..] $ lines input
---mapWithKey
+allBigger val = all (val <)
+getAllSurrounding grid key = map (fromMaybe 10) [Map.lookup (fst key - 1,snd key) grid, Map.lookup (fst key + 1, snd key) grid, Map.lookup (fst key, snd key + 1) grid, Map.lookup (fst key, snd key - 1) grid]
+btmPoints grid = sum $ map (\x->1+snd x) $ Map.toList $ Map.filterWithKey (\key value -> allBigger value (getAllSurrounding grid key)) grid
+day9 p2 input = let parsedInput = Map.fromList $ concatMap ((\line -> map (\x->((fst x,fst line),digitToInt $ snd x)) $ snd line) . Bi.second (zip [0..])) $ zip [0..] $ lines input in btmPoints parsedInput
+--filterWithKey
--- src/Helpers.hs
+++ src/Helpers.hs
@@ -1,7 +1,8 @@
-module Helpers (strToInt,taker,count,splitOn,chunksOf,group,transpose,sort,digitToInt,elemIndex,foldl',inCommon) where
+module Helpers (strToInt,taker,count,splitOn,chunksOf,group,transpose,sort,digitToInt,elemIndex,foldl',inCommon,fromMaybe) where
 import Data.List.Split (splitOn,chunksOf)
 import Data.List (group,transpose,sort,elemIndex,sort)
 import Data.Char (digitToInt)
+import Data.Maybe (fromMaybe)
 strToInt x = read x :: Int
 taker [] items = filter (/= -1) items
 taker (x:xs) items = taker xs $ take x items ++ [-1] ++ drop (x+1) items
